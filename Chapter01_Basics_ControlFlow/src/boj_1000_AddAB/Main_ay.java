a38437625542c8c958feb57ef1bd71bbc69a88aa
Ayeong <bay18200725@gmail.com>
2025-09-11 12:56:28 +0900

    ay_1000

---
+++ Chapter01_Basics_ControlFlow/src/boj_1000_AddAB/Main_ay.java
@@ -0,0 +1,14 @@
+package boj_1000_AddAB;
+import java.util.*;
+public class Main_ay {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a + b);
+
+	}
+	
+
+}
