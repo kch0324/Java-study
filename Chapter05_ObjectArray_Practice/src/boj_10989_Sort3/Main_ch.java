3876e79d6db975ee4a7bfa4b9a9e1f62a90675e2
kchan0324 <kangchan1997@gmail.com>
2025-09-18 17:56:46 +0900

    문제풀이

--- Chapter05_ObjectArray_Practice/src/boj_10989_Sort3/Main_ch.java
+++ Chapter05_ObjectArray_Practice/src/boj_10989_Sort3/Main_ch.java
@@ -1,27 +1,33 @@
 package boj_10989_Sort3;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main_ch {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();	// N: 수의 개수
+	public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());	// N: 수의 개수
 		int[] count = new int[10001];	// count: 0~10,000까지의 수를 담을 카운팅배열
 		// 입력받은 수 카운팅
 		for(int i = 0; i < N; i++) {
-			int num = sc.nextInt();
+			int num = Integer.parseInt(br.readLine());
 			count[num]++;
 		}
 		// 카운팅 배열 순회하며 개수만큼 출력
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < count.length; i++) {
 			if(count[i] == 0) {
 				continue;
 			}
 			for(int j = 0; j < count[i]; j++) {
-				System.out.println(i);
+				sb.append(i).append("\n");
 			}
 			
 		}
+		System.out.print(sb.toString());
 		
 
 	}
--- Chapter05_ObjectArray_Practice/src/boj_1181_SortWord/Main_ch.java
+++ Chapter05_ObjectArray_Practice/src/boj_1181_SortWord/Main_ch.java
@@ -1,10 +1,34 @@
 package boj_1181_SortWord;
 
+import java.util.*;
+
 public class Main_ch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // 1. 단어 개수 입력
+        int N = sc.nextInt();
 
-	}
+        // 2. HashSet으로 중복 제거
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(sc.next()); // 중복은 자동으로 제거됨
+        }
 
+        // 3. Set → 배열 또는 리스트로 변환
+        String[] arr = set.toArray(new String[0]);
+
+        // 4. 정렬: 길이 → 사전순
+        Arrays.sort(arr, (a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b);   // 길이가 같으면 사전순
+            }
+            return a.length() - b.length(); // 짧은 게 앞으로
+        });
+
+        // 5. 출력
+        for (String word : arr) {
+            System.out.println(word);
+        }
+    }
 }
