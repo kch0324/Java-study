a829f4855fb30f21be26273ac8093c63fd4ec443
chanhwi <kangchan1997@gmail.com>
2025-09-21 02:02:48 +0900

    챕터6 업로드

---
+++ Chapter06_Inheritance/src/boj_10250_ACM/Main_ch.java
@@ -0,0 +1,14 @@
+package boj_10250_ACM;
+
+import java.util.Scanner;
+
+public class Main_ch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.print(N);
+	}
+
+}
---
+++ Chapter06_Inheritance/src/boj_2869_Snail/Main_ch.java
@@ -0,0 +1,14 @@
+package boj_2869_Snail;
+
+import java.util.Scanner;
+
+public class Main_ch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.print(N);
+	}
+
+}
